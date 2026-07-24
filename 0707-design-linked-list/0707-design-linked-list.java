6a604aa8b63df1f929ec66ce12bef017fe784eb7
Mayuresh Khedkar <mayureshkhedkar05@gmail.com>
2026-07-24 16:50:46 +0530

    Time: 10 ms (47.29%), Space: 46.9 MB (56.08%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -0,0 +1,74 @@
+public class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int val){
+        this.val=val;
+    }
+}
+class MyLinkedList {
+    int size;
+    ListNode head;
+    public MyLinkedList() {
+        head=new ListNode(0);
+        size=0;
+    }
+    
+    public int get(int index) {
+        if(index<0 || index>=size)return -1;
+        ListNode curr=head;
+        for(int i=0;i<=index;i++){
+            curr=curr.next;
+        }
+        // int i=0;
+        // while(i<index){
+        //     curr=curr.next;
+        //     i++;
+        // }
+        return curr.val;
+    }
+    
+    public void addAtHead(int val) {
+        // size++;
+        // ListNode newnode=new ListNode(val);
+        // newnode.next=head;
+        // head=newnode;
+        addAtIndex(0, val);
+    }
+    
+    public void addAtTail(int val) {
+        addAtIndex(size,val);
+        
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if(index<0 || index>size)return;
+        ListNode curr=head;
+        size++;
+        for(int i=0; i<index; i++){
+            curr=curr.next;
+        }
+        ListNode newnode=new ListNode(val);
+        newnode.next=curr.next;
+        curr.next=newnode; 
+    }
+    
+    public void deleteAtIndex(int index) {
+        ListNode curr=head;
+        if(index<0 || index>=size)return;
+        for(int i =0;i<index;i++){
+            curr=curr.next;
+        }
+        size--;
+        curr.next=curr.next.next;
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
