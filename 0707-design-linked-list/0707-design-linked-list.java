86dff0b3c5fcf0aafe8488a1ceef05a2c68afa54
Mayuresh Khedkar <mayureshkhedkar05@gmail.com>
2026-07-24 17:19:02 +0530

    Time: 7 ms (96.06%), Space: 47 MB (36.84%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.java
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -1,23 +1,40 @@
-public class ListNode{
+public class ListNode {
     int val;
     ListNode next;
-    ListNode(int val){
-        this.val=val;
+    ListNode prev;
+
+    ListNode(int val) {
+        this.val = val;
     }
 }
+
 class MyLinkedList {
     int size;
     ListNode head;
+    ListNode tail;
+
     public MyLinkedList() {
-        head=new ListNode(0);
-        size=0;
+        head = new ListNode(0);
+        tail = new ListNode(0);
+        size = 0;
+        head.next = tail;
+        tail.prev = head;
     }
-    
+
     public int get(int index) {
-        if(index<0 || index>=size)return -1;
-        ListNode curr=head;
-        for(int i=0;i<=index;i++){
-            curr=curr.next;
+        if (index < 0 || index >= size)
+            return -1;
+        ListNode curr;
+        if (index < size / 2) {
+            curr = head;
+            for (int i = 0; i <= index; i++) {
+                curr = curr.next;
+            }
+        } else {
+            curr = tail.prev;
+            for (int i = 0; i < size - 1 - index; i++) {
+                curr = curr.prev;
+            }
         }
         // int i=0;
         // while(i<index){
@@ -26,41 +43,83 @@ class MyLinkedList {
         // }
         return curr.val;
     }
-    
+
     public void addAtHead(int val) {
-        // size++;
-        // ListNode newnode=new ListNode(val);
-        // newnode.next=head;
-        // head=newnode;
-        addAtIndex(0, val);
-    }
-    
-    public void addAtTail(int val) {
-        addAtIndex(size,val);
-        
-    }
-    
-    public void addAtIndex(int index, int val) {
-        if(index<0) index=0;
-        if(index>size) return;
-        ListNode curr=head;
+        ListNode newNode = new ListNode(val);
+        newNode.next = head.next;
+        newNode.prev = head;
+        head.next.prev = newNode;
+        head.next = newNode;
         size++;
-        for(int i=0; i<index; i++){
-            curr=curr.next;
-        }
-        ListNode newnode=new ListNode(val);
-        newnode.next=curr.next;
-        curr.next=newnode; 
     }
-    
-    public void deleteAtIndex(int index) {
-        ListNode curr=head;
-        if(index<0 || index>=size)return;
-        for(int i =0;i<index;i++){
-            curr=curr.next;
+
+    public void addAtTail(int val) {
+        ListNode newNode = new ListNode(val);
+        newNode.prev = tail.prev;
+        newNode.next = tail;
+        tail.prev.next = newNode;
+        tail.prev = newNode;
+        size++;
+
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index < 0)
+            index = 0;
+        if (index > size)
+            return;
+
+        if (index == 0) {
+            addAtHead(val);
+            return;
         }
+        if (index == size) {
+            addAtTail(val);
+            return;
+        }
+        ListNode curr ;
+        size++;
+        if (index < size / 2) {
+            curr = head;
+            for (int i = 0; i < index; i++) {
+                curr = curr.next;
+            }
+        } else {
+            curr = tail.prev;
+            for (int i = 0; i < size - 1 - index; i++) {
+                curr = curr.prev;
+            }
+        }
+
+        ListNode newnode = new ListNode(val);
+        // newnode.next = curr.next;
+        // newnode.prev=curr.next.prev;
+        // curr.next = newnode;
+        newnode.next = curr.next;
+        newnode.prev = curr;
+        curr.next.prev = newnode;
+        curr.next = newnode;
+    }
+
+    public void deleteAtIndex(int index) {
+       if (index < 0 || index >= size) return;
+
+        // Find target node to delete
+        ListNode curr;
+        if (index < size / 2) {
+            curr = head.next;
+            for (int i = 0; i < index; i++) {
+                curr = curr.next;
+            }
+        } else {
+            curr = tail.prev;
+            for (int i = 0; i < size - 1 - index; i++) {
+                curr = curr.prev;
+            }
+        }
+        curr.prev.next = curr.next;
+        curr.next.prev = curr.prev;
         size--;
-        curr.next=curr.next.next;
     }
 }
 
