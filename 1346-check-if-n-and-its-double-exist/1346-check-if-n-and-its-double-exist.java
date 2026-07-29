bf7ebc0be37598296a69099bd19a832fd82da934
Mayuresh Khedkar <mayureshkhedkar05@gmail.com>
2026-07-29 10:16:13 +0530

    Time: 3 ms (26.76%), Space: 46.4 MB (5.28%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
         HashSet<Integer> set1 = new HashSet<>();
-int zerocount=0;
+        int zerocount = 0;
         for (int i = 0; i < arr.length; i++) {
             set1.add(arr[i]);
             if (arr[i] == 0) {
                 zerocount++;
             }
         }
-        if(zerocount>=2) return true;
+        if (zerocount >= 2)
+            return true;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 0)
                 continue;
