0ec2955945f002cbb78253cd6258523203848a21
Mayuresh Khedkar <mayureshkhedkar05@gmail.com>
2026-07-30 14:09:56 +0530

    Time: 4 ms (37.05%), Space: 43.4 MB (25.94%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()==1||s.length()==0)return false;
+        Deque<Character> st= new ArrayDeque<>();
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('||s.charAt(i)=='['||s.charAt(i)=='{'){
+                st.push(s.charAt(i));
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char top=st.pop();
+                if (( s.charAt(i)== ')' && top!= '(') ||
+                    (s.charAt(i) == ']' && top!= '[') ||
+                    (s.charAt(i) == '}' && top!= '{')) {
+                    return false;
+                }
+            }
+            
+        }
+        return st.isEmpty();
+    }
+}
